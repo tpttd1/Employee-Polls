ccc54ab27c8235f5c73c6034df4c9b30b32654c3
TiemHV <TiemHV@fsoft.com.vn>
2022-11-10 21:26:47 +0700

    update

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -13,11 +13,11 @@ const Question = (props) => {
   } = props;
 
   useEffect(() => {
-    if (user && user?.answers) {
+    if (user) {
       const newPoll = [];
       const donePoll = [];
 
-      const answers = Object.keys(user.answers);
+      const answers = user?.answers ? Object.keys(user.answers) : [];
       questions.forEach((question) => {
         if (answers.includes(question.id)) {
           donePoll.push(question);
